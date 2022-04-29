cc68a47f2707405591e0c751ed1cd9a648623e6d
MaryamHooshyari <mhshyr96@gmail.com>
2022-04-29 16:13:53 +0430

    apps added

--- courier_salary/settings.py
+++ courier_salary/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
+    'courier',
+    'income',
+    'salary',
 ]
 
 MIDDLEWARE = [
